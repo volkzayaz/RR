92647e085884412b52337e7126623c5d7503abcc
Alexander Obolentsev <alexander@olearis.com>
2018-09-19 17:37:13 +0300

    Fix for IOS-168 iPhone iOS 11/12: Player additional menu items cannot be invoked after they were opened in offline

--- RhythmicRebellion/Source/UI Components/ErrorPresenting.swift
+++ RhythmicRebellion/Source/UI Components/ErrorPresenting.swift
@@ -21,8 +21,8 @@ extension ErrorPresenting where Self: UIViewController {
 
     func show(error: Error, completion: (() -> Void)? = nil) {
         let errorAlertController = UIAlertController(title: nil, message: error.localizedDescription, preferredStyle: .alert)
-        errorAlertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "OK Title for AlertAction"), style: .cancel, handler: { (action) in
-        errorAlertController.dismiss(animated: true, completion: nil)
+        errorAlertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "OK Title for AlertAction"), style: .cancel, handler: { [weak errorAlertController] (action) in
+        errorAlertController?.dismiss(animated: true, completion: nil)
         }))
 
         self.present(errorAlertController, animated: true, completion: completion)
