530580787d8c30e9b4a8ceb91942fdf10ea15505
Alexander Obolentsev <alexander@olearis.com>
2018-11-01 19:04:46 +0200

    Start supporting new image types and Base64 in path

--- RhythmicRebellion/Source/Models/Image.swift
+++ RhythmicRebellion/Source/Models/Image.swift
@@ -10,28 +10,70 @@ import Foundation
 import CoreGraphics
 
 enum ImageLinkType: String {
+    case large
+    case xlarge
+    case medium
+    case original
+    case preload
+    case small
+    case xsmall
+
     case thumb
     case big
 }
 
 struct ImageLink: Codable {
 
-    let urlString: String
+    enum PathType {
+        case url(String)
+        case base64(String)
+
+        var string: String {
+            switch self {
+            case .url(let urlString): return urlString
+            case .base64(let base64String): return base64String
+            }
+        }
+    }
+
+    let path: PathType
     let width: Float
     let height: Float
 
     var size: CGSize { return CGSize(width: CGFloat(width), height: CGFloat(height)) }
 
     enum CodingKeys: String, CodingKey {
-        case urlString = "path"
+        case path
         case width
         case height
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        let pathString = try container.decode(String.self, forKey: .path)
+        if pathString.starts(with: "data") {
+            self.path = .base64(pathString)
+        } else {
+            self.path = .url(pathString)
+        }
+
+        self.width = try container.decode(Float.self, forKey: .width)
+        self.height = try container.decode(Float.self, forKey: .height)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(self.path.string, forKey: .path)
+        try container.encode(self.width, forKey: .width)
+        try container.encode(self.height, forKey: .height)
     }
 }
 
 struct Image: Codable {
 
-    let uuid: String?
+    let uuid: String
     let links: [ImageLinkType.RawValue: ImageLink]
 
     enum CodingKeys: String, CodingKey {
@@ -42,7 +84,7 @@ struct Image: Codable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
 
-        self.uuid = try container.decodeIfPresent(String.self, forKey: .uuid)
+        self.uuid = try container.decode(String.self, forKey: .uuid)
         self.links = try container.decode([ImageLinkType.RawValue: ImageLink].self, forKey: .links)
     }
 
@@ -56,13 +98,11 @@ struct Image: Codable {
 
 extension Image: Equatable {
     static func == (lhs: Image, rhs: Image) -> Bool {
-        guard let lhsuuid = lhs.uuid, let rhsuuid = rhs.uuid else { return false }
-        return lhsuuid == rhsuuid
+        return lhs.uuid == rhs.uuid
     }
 }
 
-//extension Image: Hashable {
-//
-//    public var hashValue: Int { return self.uuid.hashValue }
-//}
+extension Image: Hashable {
+    public var hashValue: Int { return self.uuid.hashValue }
+}
 
