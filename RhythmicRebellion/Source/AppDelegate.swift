561df6e8ebbd0f085da608eebb02791e3e336426
Vlad Soroka <volkzayaz@gmail.com>
2019-03-02 18:51:39 +0200

    Finished fast lane + crashlytics set up

--- RhythmicRebellion/Source/AppDelegate.swift
+++ RhythmicRebellion/Source/AppDelegate.swift
@@ -62,11 +62,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         FirebaseApp.configure()
-//
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-//            Crashlytics.sharedInstance().crash()
-//        }
-        
+
         Fabric.with([Crashlytics.self])
         
         
