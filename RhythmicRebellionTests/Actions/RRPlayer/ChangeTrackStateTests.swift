37eb998b03a52ecdeee2f8fe820048feb2d99dd3
Andrey Ivanov <this.andrey@gmail.com>
2019-05-14 17:43:56 +0300

    added test for ChangeTrackState

--- RhythmicRebellionTests/Actions/RRPlayer/ChangeTrackStateTests.swift
+++ RhythmicRebellionTests/Actions/RRPlayer/ChangeTrackStateTests.swift
@@ -23,7 +23,32 @@ class ChangeTrackStateTests: XCTestCase {
     }
     
     func testChangeTrackState() {
-//        Dispatcher.dispatch(action: ChangeTrackState())
+        
+        let tracks = Tracks.all
+        Dispatcher.dispatch(action: InsertTracks(tracks: tracks, afterTrack: nil))
+        expect(player.tracks.count).toEventually(equal(tracks.count))
+        
+        let firstOrderedTrack = orderedTracks[0]
+        //Mock Requests
+        let addonUrl = try! TrackRequest.addons(trackIds: [firstOrderedTrack.track.id]).asURLRequest().url!
+        FakeRequests.Addons.registerAdvertisementAddon(with: addonUrl)
+        
+        let artistUrl = try! TrackRequest.artist(artistId: firstOrderedTrack.track.artist.id).asURLRequest().url!
+        FakeRequests.registerMockRequestArtist(with: artistUrl)
+        
+        expect(player.currentItem).to(beNil())
+        //Prepare new track
+        Dispatcher.dispatch(action: PrepareNewTrack(orderedTrack: firstOrderedTrack, shouldPlayImmidiatelly: true))
+        expect(player.currentItem).toNotEventually(beNil())
+        
+        expect(player.currentItem?.state.progress) == 0
+        expect(player.currentItem?.state.isPlaying) == true
+        
+        let newState = TrackState(progress: 5, isPlaying: false, skipSeek: ())
+        Dispatcher.dispatch(action: ChangeTrackState(trackState: newState))
+
+        expect(player.currentItem?.state.progress) == 5
+        expect(player.currentItem?.state.isPlaying) == false
     }
 }
 
